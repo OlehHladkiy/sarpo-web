8ca393949de78faa22c69269bbc0a22df6bf1e71
Oleg <oleg.h@develops.today>
2020-05-05 13:48:37 +0300

    feat(store): configure store

---
+++ src/store/configureStore.ts
@@ -0,0 +1,52 @@
+import { routerMiddleware } from 'connected-react-router';
+import { createBrowserHistory } from 'history';
+import { applyMiddleware, compose, createStore } from 'redux';
+import { persistStore } from 'redux-persist';
+import createSagaMiddleware from 'redux-saga';
+import thunk from 'redux-thunk';
+
+import graphqlClient from '@graphql/client';
+import graphqlMiddleware from '@graphql/middlewares/graphqlMiddleware';
+
+import axiosMiddleware from './middlewares/axiosMiddleware';
+import loggerMiddleware from './middlewares/loggerMiddleware';
+import promiseMiddleware from './middlewares/promiseMiddleware';
+import rootReducer from './rootReducer';
+import rootSaga from './rootSaga';
+
+export default (initialState = {}) => {
+  const history = createBrowserHistory();
+  const sagaMiddleware = createSagaMiddleware();
+
+  const middlewares = [
+    routerMiddleware(history),
+    axiosMiddleware,
+    graphqlMiddleware(graphqlClient),
+    thunk,
+    promiseMiddleware,
+    sagaMiddleware,
+  ];
+
+  if (typeof jest === 'undefined' && process.env.NODE_ENV !== 'production') {
+    middlewares.push(loggerMiddleware);
+  }
+
+  const store = createStore(
+    rootReducer(history),
+    initialState,
+    compose(applyMiddleware(...middlewares))
+  );
+  const persistor = persistStore(store);
+
+  sagaMiddleware.run(rootSaga);
+
+  if (module.hot) {
+    module.hot.accept('./rootReducer', () => {
+      // eslint-disable-next-line global-require
+      const nextRootReducer = require('./rootReducer').default(history);
+      store.replaceReducer(nextRootReducer);
+    });
+  }
+
+  return { history, persistor, store };
+};
