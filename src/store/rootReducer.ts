c26816e0dbb6752c97cdfc2eb56034e9e761b39b
Oleg <oleg.h@develops.today>
2020-05-07 19:18:45 +0300

    feat(store): update root reducer

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -5,13 +5,13 @@ import { persistReducer } from 'redux-persist';
 
 import ReduxPersist from '@config/ReduxPersist';
 import AuthReducer, {
-  STATE_KEY as AUTH_STATE_KEY
+  STATE_KEY as AUTH_STATE_KEY,
 } from '@modules/auth/AuthReducer';
 import RouterReducer, {
-  STATE_KEY as ROUTER_STATE_KEY
+  STATE_KEY as ROUTER_STATE_KEY,
 } from '@modules/router/RouterReducer';
 import UserReducer, {
-  STATE_KEY as USER_STATE_KEY
+  STATE_KEY as USER_STATE_KEY,
 } from '@modules/user/UserReducer';
 
 const persist = R.curry(persistReducer)(ReduxPersist.storeConfig);
@@ -22,5 +22,5 @@ export default (history: any): Reducer<any, any> =>
     combineReducers,
     R.assoc(AUTH_STATE_KEY, AuthReducer),
     R.assoc(ROUTER_STATE_KEY, RouterReducer(history)),
-    R.assoc(USER_STATE_KEY, UserReducer)
+    R.assoc(USER_STATE_KEY, UserReducer),
   )({});
