10e51d917b87d98d75d2a769e88ed629d93b451e
Oleg <oleg.h@develops.today>
2020-05-07 19:04:17 +0300

    feat(store): update root reducer

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,25 +1,26 @@
+import { Reducer } from 'react';
 import * as R from 'ramda';
 import { combineReducers } from 'redux';
 import { persistReducer } from 'redux-persist';
 
 import ReduxPersist from '@config/ReduxPersist';
 import AuthReducer, {
-  STATE_KEY as AUTH_STATE_KEY,
+  STATE_KEY as AUTH_STATE_KEY
 } from '@modules/auth/AuthReducer';
 import RouterReducer, {
-  STATE_KEY as ROUTER_STATE_KEY,
+  STATE_KEY as ROUTER_STATE_KEY
 } from '@modules/router/RouterReducer';
 import UserReducer, {
-  STATE_KEY as USER_STATE_KEY,
+  STATE_KEY as USER_STATE_KEY
 } from '@modules/user/UserReducer';
 
 const persist = R.curry(persistReducer)(ReduxPersist.storeConfig);
 
-export default (history: any) =>
+export default (history: any): Reducer<any, any> =>
   R.compose(
     persist,
     combineReducers,
     R.assoc(AUTH_STATE_KEY, AuthReducer),
     R.assoc(ROUTER_STATE_KEY, RouterReducer(history)),
-    R.assoc(USER_STATE_KEY, UserReducer),
+    R.assoc(USER_STATE_KEY, UserReducer)
   )({});
