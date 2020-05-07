be821c001a065f42f49998c3c6faa9c0e560e559
Oleg <oleg.h@develops.today>
2020-05-07 18:26:09 +0300

    feat(config): add app config

--- src/config/AppConfig.ts
+++ src/config/AppConfig.ts
@@ -1,8 +1,5 @@
 import * as R from 'ramda';
 
 export default {
-  apiUrl: R.defaultTo(
-    'http://localhost:4001',
-    process.env.SARPO_API_URL,
-  ),
+  apiUrl: R.defaultTo('http://localhost:4001', process.env.SARPO_API_URL),
 };
