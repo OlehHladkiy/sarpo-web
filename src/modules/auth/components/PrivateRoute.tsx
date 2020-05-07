7aae745eea405d40974c8e623fcadb2a87fcae20
Oleg <oleg.h@develops.today>
2020-05-07 18:27:39 +0300

    feat(auth): implement private route

---
+++ src/modules/auth/components/PrivateRoute.tsx
@@ -0,0 +1,42 @@
+// @flow
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+// eslint-disable-next-line no-unused-vars
+import { Redirect, Route, useHistory, RouteProps } from 'react-router-dom';
+
+import { getIsAuthenticated } from '../AuthReducer';
+
+interface PrivateRouteProps extends RouteProps {
+  // eslint-disable-next-line
+  component: any;
+}
+
+const PrivateRoute: React.SFC<PrivateRouteProps> = ({
+  component: Component,
+  ...rest
+}: PrivateRouteProps) => {
+  const history = useHistory();
+  const isAuthenticated = useSelector(getIsAuthenticated);
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      history.push('/signIn');
+      return;
+    }
+  }, [history, isAuthenticated]);
+
+  return (
+    <Route
+      {...rest}
+      render={(props): React.ReactNode => {
+        if (isAuthenticated) {
+          return <Component {...props} />;
+        }
+
+        return <Redirect to="/signIn" />;
+      }}
+    />
+  );
+};
+
+export default PrivateRoute;
