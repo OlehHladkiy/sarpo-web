68a1b757cd727c90f9f6d8fe195f22bd4477c215
Oleg <oleg.h@develops.today>
2020-05-11 00:46:24 +0300

    feat(modules): implement dashboard and conferences modules

---
+++ src/modules/conference/pages/ConferencesPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import LayoutHeader from '@components/LayoutHeader';
+
+const ConferencesPage: React.FunctionComponent = () => {
+  return (
+    <div>
+      <LayoutHeader />
+    </div>
+  );
+};
+
+export default ConferencesPage;
---
+++ src/modules/dashboard/pages/DashboardPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import Layout from '@components/Layout';
+
+const DashboardPage: React.FunctionComponent = () => {
+  return <Layout>dashboard</Layout>;
+};
+
+export default DashboardPage;
