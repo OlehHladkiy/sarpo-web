1ed1ce4a22ab9ab52a367482016e7638895f29b1
Oleg <oleg.h@develops.today>
2020-05-11 00:48:55 +0300

    feat(auth): implement sign in view to sign in page

---
+++ src/modules/auth/components/SignInPageView.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { FormInstance } from 'antd/lib/form';
+import styled from 'styled-components';
+
+import SignInForm from '../forms/SignInForm';
+import OnboardingWarning from './OnboardingWarning';
+
+interface SignInPageViewProps {
+  form: FormInstance;
+  onFinish: (values: Record<string, any>) => void;
+}
+
+const SignInPageView: React.FunctionComponent<SignInPageViewProps> = ({
+  form,
+  onFinish,
+}: SignInPageViewProps) => (
+  <Wrapper>
+    <LogoWrapper>
+      <Logo />
+    </LogoWrapper>
+    <Title>Sign in</Title>
+    <Tip>Use email to get started</Tip>
+    <FormWrapper>
+      <SignInForm form={form} onFinish={onFinish} />
+    </FormWrapper>
+    <OnboardingWarning />
+  </Wrapper>
+);
+
+const Wrapper = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  input {
+    height: 45px;
+  }
+`;
+
+const LogoWrapper = styled.div`
+  margin-top: 70px;
+  margin-bottom: 24px;
+`;
+
+const Logo = styled.div`
+  height: 100px;
+  width: 150px;
+  background-image: url('/images/main-logo.png');
+  background-size: 60%;
+  background-repeat: no-repeat;
+  background-position: center;
+`;
+
+const Title = styled.div`
+  padding-bottom: 40px;
+  font-size: 1.5rem;
+`;
+
+const Tip = styled.div`
+  color: #6f7287;
+  font-size: 0.875rem;
+`;
+
+const FormWrapper = styled.div`
+  margin-top: 40px;
+  width: 27%;
+`;
+
+export default SignInPageView;
--- src/modules/auth/pages/SignInPage.tsx
+++ src/modules/auth/pages/SignInPage.tsx
@@ -1,72 +1,24 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { Form } from 'antd';
+import { Form, message as notify } from 'antd';
 import React from 'react';
-import styled from 'styled-components';
+import { useDispatch } from 'react-redux';
 
-import SignInForm from '../forms/SignInForm';
-import OnboardingWarning from '../components/OnboardingWarning';
+import { signIn } from '../AuthActions';
+import SignInPageView from '../components/SignInPageView';
 
 const SignInPage: React.FunctionComponent = () => {
   const [form] = Form.useForm();
 
-  const onFinish = (values: Record<string, any>): void => {
-    console.log(values);
+  const dispatch = useDispatch();
+
+  const onFinish = async (values: Record<string, any>): Promise<void> => {
+    notify.loading('Signing In...');
+
+    await dispatch(signIn(values));
+
+    notify.destroy();
   };
 
-  return (
-    <Wrapper>
-      <LogoWrapper>
-        <Logo />
-      </LogoWrapper>
-      <Title>Sign in</Title>
-      <Tip>Use email to get started</Tip>
-      <FormWrapper>
-        <SignInForm form={form} onFinish={onFinish} />
-      </FormWrapper>
-      <OnboardingWarning />
-    </Wrapper>
-  );
+  return <SignInPageView form={form} onFinish={onFinish} />;
 };
-
-const Wrapper = styled.div`
-  height: 100%;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-
-  input {
-    height: 45px;
-  }
-`;
-
-const LogoWrapper = styled.div`
-  margin-top: 70px;
-  margin-bottom: 24px;
-`;
-
-const Logo = styled.div`
-  height: 100px;
-  width: 150px;
-  background-image: url('/images/main-logo.png');
-  background-size: 60%;
-  background-repeat: no-repeat;
-  background-position: center;
-`;
-
-const Title = styled.div`
-  padding-bottom: 40px;
-  font-size: 1.5rem;
-`;
-
-const Tip = styled.div`
-  color: #6f7287;
-  font-size: 0.875rem;
-`;
-
-const FormWrapper = styled.div`
-  margin-top: 40px;
-  width: 27%;
-`;
 
 export default SignInPage;
