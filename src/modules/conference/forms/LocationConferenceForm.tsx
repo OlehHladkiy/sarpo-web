8c7dc8cd94f0228849f090071f6b988d0faa0d7f
Oleg <oleg.h@develops.today>
2020-05-12 00:55:26 +0300

    feat(conference): implement location conference form

---
+++ src/modules/conference/forms/LocationConferenceForm.tsx
@@ -0,0 +1,41 @@
+import { Form, Radio, Input } from 'antd';
+import React, { useState } from 'react';
+import { EnvironmentOutlined } from '@ant-design/icons';
+
+import FormLayout from '../components/FormLayout';
+import { ConferenceType } from '../models/conference';
+
+const LocationConferenceForm: React.FunctionComponent = () => {
+  const [type, setType] = useState(ConferenceType.Venue);
+
+  const getComponentByType = (): React.ReactNode => {
+    switch (type) {
+      case ConferenceType.Venue: {
+        return <Input placeholder="Enter address" />;
+      }
+      default: {
+        return null;
+      }
+    }
+  };
+
+  return (
+    <FormLayout
+      title="Location"
+      description="Help people in the area discover your event and let attendees know where to show up."
+      icon={<EnvironmentOutlined />}
+    >
+      <Form.Item name="type" shouldUpdate={true}>
+        <Radio.Group onChange={(e): void => setType(e.target.value)}>
+          <Radio.Button value={ConferenceType.Venue}>Venue</Radio.Button>
+          <Radio.Button value={ConferenceType.Online}>
+            Online conference
+          </Radio.Button>
+        </Radio.Group>
+      </Form.Item>
+      <Form.Item>{getComponentByType()}</Form.Item>
+    </FormLayout>
+  );
+};
+
+export default LocationConferenceForm;
