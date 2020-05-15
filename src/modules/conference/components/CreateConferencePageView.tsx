0dc2bdef35dda6b7bf82e41811f8b61b75a37387
Oleg <oleg.h@develops.today>
2020-05-15 03:34:22 +0300

    feat(conference): implement create conference page view component

---
+++ src/modules/conference/components/CreateConferencePageView.tsx
@@ -0,0 +1,46 @@
+import { FormInstance } from 'antd/lib/form';
+import React from 'react';
+import styled from 'styled-components';
+
+import LayoutHeader from '@components/LayoutHeader';
+
+import CreateOrEditConferenceForm from '../forms/CreateOrEditConferenceForm';
+
+interface CreateConferencePageViewProps {
+  form: FormInstance;
+  onFinish: (values: Record<string, any>) => Promise<void>;
+}
+
+const CreateConferencePageView: React.FunctionComponent<CreateConferencePageViewProps> = ({
+  form,
+  onFinish,
+}: CreateConferencePageViewProps) => (
+  <Wrapper>
+    <LayoutHeader />
+    <FormWrapper>
+      <CreateOrEditConferenceForm form={form} onFinish={onFinish} />
+    </FormWrapper>
+  </Wrapper>
+);
+
+const Wrapper = styled.div`
+  margin-bottom: 40px;
+`;
+
+const FormWrapper = styled.div`
+  margin: 0 25%;
+
+  input {
+    height: 45px;
+  }
+
+  @media (max-width: 768px) {
+    margin: 0 40%;
+  }
+
+  @media (max-width: 1024px) {
+    margin: 0 15%;
+  }
+`;
+
+export default CreateConferencePageView;
