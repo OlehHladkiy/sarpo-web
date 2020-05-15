9b56e06575f2dba1013f1ae38d3003a5ff136886
Oleg <oleg.h@develops.today>
2020-05-15 03:36:07 +0300

    feat(conference): update date conference form

--- src/modules/conference/forms/DateConferenceForm.tsx
+++ src/modules/conference/forms/DateConferenceForm.tsx
@@ -1,4 +1,4 @@
-import { Radio, Form } from 'antd';
+import { Radio, Form, DatePicker, TimePicker } from 'antd';
 import React from 'react';
 import { FieldTimeOutlined } from '@ant-design/icons';
 import styled from 'styled-components';
@@ -6,27 +6,46 @@ import styled from 'styled-components';
 import FormLayout from '../components/FormLayout';
 import { ConferenceDateType } from '../models/conference';
 
-const DateConferenceForm: React.FunctionComponent = () => (
-  <FormLayout
-    title="Date and time"
-    description="Tell event-goers when your event starts and ends so they can make plans to attend"
-    icon={<FieldTimeOutlined />}
-  >
-    <FrequencyTitle>Frequency</FrequencyTitle>
-    <Form.Item name="dateType">
-      <Radio.Group>
-        <Radio value={ConferenceDateType.Single}>
-          Single Event - Happens once and can last multiple days
-        </Radio>
-        <Radio value={ConferenceDateType.Multiple}>
-          Recurring Events - Repeats or occurs more than once
-        </Radio>
-      </Radio.Group>
-    </Form.Item>
-  </FormLayout>
-);
+const DateConferenceForm: React.FunctionComponent = () => {
+  return (
+    <FormLayout
+      title="Date and time"
+      description="Tell event-goers when your event starts and ends so they can make plans to attend"
+      icon={<FieldTimeOutlined />}
+    >
+      <Title>Frequency</Title>
+      <Form.Item name="dateType">
+        <Radio.Group>
+          <Radio value={ConferenceDateType.Single}>
+            Single Event - Happens once and can last multiple days
+          </Radio>
+          <Radio value={ConferenceDateType.Multiple}>
+            Recurring Events - Repeats or occurs more than once
+          </Radio>
+        </Radio.Group>
+      </Form.Item>
+      <Title>Start and end times</Title>
+      <Form.Item
+        name="date"
+        rules={[{ required: true, message: 'Please select the date' }]}
+      >
+        <DatePicker.RangePicker autoComplete="off" />
+      </Form.Item>
+      <Form.Item
+        name="time"
+        rules={[{ required: true, message: 'Please select the time' }]}
+      >
+        <TimePicker.RangePicker
+          autoComplete="off"
+          picker="time"
+          format="HH:mm"
+        />
+      </Form.Item>
+    </FormLayout>
+  );
+};
 
-const FrequencyTitle = styled.div`
+const Title = styled.div`
   margin-top: 24px;
   margin-bottom: 12px;
   font-size: 0.875rem;
