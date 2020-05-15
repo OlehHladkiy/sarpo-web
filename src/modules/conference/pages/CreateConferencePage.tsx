3a25151881327a59e948a04d2625b46e11768187
Oleg <oleg.h@develops.today>
2020-05-15 03:39:26 +0300

    feat(store): implement logic to create conference

--- src/modules/conference/pages/CreateConferencePage.tsx
+++ src/modules/conference/pages/CreateConferencePage.tsx
@@ -1,23 +1,23 @@
 import { Form } from 'antd';
 import React from 'react';
+import { useDispatch } from 'react-redux';
 
-import LayoutHeader from '@components/LayoutHeader';
-
-import CreateOrEditConferenceForm from '../forms/CreateOrEditConferenceForm';
+import { normalizeBasicConferenceToCreate } from '../utils/conference-helpers';
+import { createConference } from '../ConferenceActions';
+import CreateConferencePageView from '../components/CreateConferencePageView';
 
 const CreateConferencePage: React.FunctionComponent = () => {
   const [form] = Form.useForm();
 
-  const onFinish = (values: Record<string, any>): void => {
-    console.log(values);
+  const dispatch = useDispatch();
+
+  const onFinish = async (values: Record<string, any>): Promise<void> => {
+    const normalizedData = normalizeBasicConferenceToCreate(values);
+
+    await dispatch(createConference(normalizedData));
   };
 
-  return (
-    <div>
-      <LayoutHeader />
-      <CreateOrEditConferenceForm form={form} onFinish={onFinish} />
-    </div>
-  );
+  return <CreateConferencePageView form={form} onFinish={onFinish} />;
 };
 
 export default CreateConferencePage;
