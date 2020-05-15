00a7c2d863d86c4859738b8a89f9b403693e7256
Oleg <oleg.h@develops.today>
2020-05-15 03:37:04 +0300

    feat(conference): implement logic to fetch conferences on list page

--- src/modules/conference/pages/ConferencesPage.tsx
+++ src/modules/conference/pages/ConferencesPage.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-import LayoutHeader from '@components/LayoutHeader';
+import ConferencesPageView from '../components/ConferencesPageView';
+import { fetchConferences } from '../ConferenceActions';
+import { getConferences } from '../ConferenceReducer';
 
 const ConferencesPage: React.FunctionComponent = () => {
-  return (
-    <div>
-      <LayoutHeader />
-    </div>
-  );
+  const conferences = useSelector(getConferences);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchConferences());
+  }, []);
+
+  return <ConferencesPageView conferences={conferences} />;
 };
 
 export default ConferencesPage;
