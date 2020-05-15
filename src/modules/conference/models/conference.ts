ad264a25e76ca3e076341f5c73dd2ded9ed38e2a
Oleg <oleg.h@develops.today>
2020-05-15 03:36:41 +0300

    feat(conference): implement enum for onboarding

--- src/modules/conference/models/conference.ts
+++ src/modules/conference/models/conference.ts
@@ -7,3 +7,9 @@ export enum ConferenceDateType {
   Single = 'single',
   Multiple = 'multiple',
 }
+
+export enum VenueConferenceOnboardingStep {
+  Basic = 'basic',
+  Details = 'details',
+  Tickets = 'tickets',
+}
